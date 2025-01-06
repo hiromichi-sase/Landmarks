5f1116649b2e623deb3a3b8f581caf79d7ade036
Hiro <hiromichi.sase@gmail.com>
2025-01-06 11:49:46 +0900

    Creating and combining views Section 2

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, SwiftUI!")
+            Text("Turtle Rock")
+                .font(.title)
         }
-        .padding()
     }
 }
 
