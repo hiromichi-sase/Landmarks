ab1952ba7db45660eed4b48e7d8af6975e9cf97d
Hiro <hiromichi.sase@gmail.com>
2025-01-10 10:53:48 +0900

    Composing complex interfaces Section 3

---
+++ Landmarks/Views/Categories/CategoryItem.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryItem.swift
+//  Landmarks
+//
+//  Created by Hiromichi Sase on 2025/01/10.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .scaledToFit()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
