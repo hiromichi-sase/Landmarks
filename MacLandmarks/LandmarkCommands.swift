eef4f002c6ab2c5102ce2b440032366efde0ec0b
Hiro <hiromichi.sase@gmail.com>
2025-01-16 16:52:12 +0900

    Creating a macOS app Section 5

---
+++ MacLandmarks/LandmarkCommands.swift
@@ -0,0 +1,14 @@
+//
+//  LandmarkCommands.swift
+//  Landmarks
+//
+//  Created by Hiromichi Sase on 2025/01/16.
+//
+
+import SwiftUI
+
+struct LandmarkCommands: Commands {
+    var body: some Commands {
+        SidebarCommands()
+    }
+}
